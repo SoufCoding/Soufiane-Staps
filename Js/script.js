dfe5c6b0f2776ba56f87fc18c9ff5e84f4270a0b
SoufCoding <75219279+SoufCoding@users.noreply.github.com>
2022-04-27 14:12:09 +0100

    N

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+var typed = new Typed('.typing',{
+    strings:["Personal Trainer","Karate Coach","Fitness Coach","HandBall Athlete"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+})
